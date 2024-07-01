0b6158cd0a179506c21f62632bbf77aea8b02aa8
wilson Blanco <blancowilson83@gmail.com>
2024-06-30 23:48:53 -0400

    login y creacion de usuario, trabajando en CRUD

--- app.py
+++ app.py
@@ -5,10 +5,28 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        return render_template('./templates/index.html')
+        return render_template('./index.html')
     except Exception as e:
         # Handle the exception here or log it for debugging purposes
         # You can also return an error page or a custom error message
         return 'An error occurred: {}'.format(str(e)), 500
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/proyectos')
+def proyectos():
+    return render_template('proyectos.html')
+
+@app.route('/formulario_contacto')
+def formulario_contacto():
+    return render_template('formulario_contacto.html')
+
+@app.route('/tarjeta_incidente')
+def tarjeta_incidente():
+    return render_template('tarjeta_incidente.html')
+
+
 if __name__=='__main__':
  app.run(debug=True)
