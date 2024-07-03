10df232133c96e5a129f06f1e2005551a6db9085
jorgempereyra <46533043+jorgempereyra@users.noreply.github.com>
2024-07-03 01:04:42 -0300

    Version_python_anywhere

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 # config.py
 import os
 
+#class Config:
+#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+#    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ugbpggmt7bpmnts1:uQFy4bXcUYnIBCIyLKJt@bvscsukiuuob2dodepv4-mysql.services.clever-cloud.com/bvscsukiuuob2dodepv4'
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ugbpggmt7bpmnts1:uQFy4bXcUYnIBCIyLKJt@bvscsukiuuob2dodepv4-mysql.services.clever-cloud.com/bvscsukiuuob2dodepv4'
+#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://jpereyra:CODOACODO2024@jpereyra.mysql.pythonanywhere-services.com/jpereyra$MegaTask'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
